3ff569128d1abafab13c19e375662433ce31c18b
pchurzin <pchurzin@pchurzin.ru>
2018-11-29 12:30:48 +0300

    task 1

---
+++ src/Task001.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Все ли символы в строке встречаются только один раз
+ * https://t.me/unilecs/4
+ */
+
+public class Task001 {
+
+    public static void main(String[] args) {
+        System.out.println(checkUniq("abcdea"));
+        System.out.println(checkUniq("abcde"));
+    }
+
+    private static boolean checkUniq(String str) {
+        Map<Character, Integer> charCounter = new HashMap<>();
+        for (Character c : str.toCharArray()) {
+            Integer count = charCounter.merge(c, 1, (old, val) -> old + 1);
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
