83197fa4d019fc7c2e909e3c48a9766b4ab98a5e
pchurzin <pchurzin@pchurzin.ru>
2018-12-11 13:25:31 +0300

    task 5

---
+++ src/Task005.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * https://t.me/unilecs/10
+ * можно ли сделать строку палиндромом
+ */
+public class Task005 {
+    public static void main(String[] args) {
+        System.out.println(canBePalindrome("bbo"));//true
+        System.out.println(canBePalindrome("bob"));//true
+        System.out.println(canBePalindrome("cat"));//false
+    }
+
+    private static boolean canBePalindrome(String s) {
+        Objects.requireNonNull(s);
+        if (s.length() < 3) {
+            return true;
+        }
+
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charCount.merge(c, 1, Integer::sum);
+        }
+        boolean hasOddCount = false;
+        for (int i : charCount.values()) {
+            if (i % 2 != 0) {
+                if (hasOddCount) return false;
+                hasOddCount = true;
+            }
+        }
+        return true;
+    }
+}
