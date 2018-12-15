2110b4ebad331fc066c806a344eb3cd7efd6740e
pchurzin <pchurzin@pchurzin.ru>
2018-12-15 11:52:54 +0300

    task 9

---
+++ src/Task009.java
@@ -0,0 +1,24 @@
+/**
+ * https://t.me/unilecs/14
+ * <p>
+ * Напишите программу, которая будет печатать числа Фибоначчи
+ * максимально долго (без ошибок времени выполнения)
+ */
+public class Task009 {
+    public static void main(String[] args) {
+        fib();
+    }
+
+    private static void fib() {
+        int n1 = 0;
+        int n2 = 1;
+        int f = 0;
+        while (Integer.MAX_VALUE - f > n1) { //to eliminate overflow
+            System.out.println(f);
+            n1 = n2;
+            n2 = f;
+            f = n1 + n2;
+        }
+        System.out.println(f);
+    }
+}
