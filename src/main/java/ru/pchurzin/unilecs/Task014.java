66ca533740a60d3f0972f64ceed5953d5d3c3945
Pavel Churzin <pchurzin@pchurzin.ru>
2019-04-02 17:45:02 +0300

    task14 using iterator

--- src/main/java/ru/pchurzin/unilecs/Task014.java
+++ src/main/java/ru/pchurzin/unilecs/Task014.java
@@ -1,10 +1,10 @@
 package ru.pchurzin.unilecs;
 
-import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * https://t.me/unilecs/28
- *
+ * <p>
  * Вывести слова в строке в обратном порядке
  */
 
@@ -17,4 +17,54 @@ public class Task014 {
         }
         return result;
     }
+
+    static class WordReverser implements Iterable<String> {
+        private char[] string;
+
+        WordReverser(char[] input) {
+            this.string = input;
+        }
+
+        @Override
+        public Iterator<String> iterator() {
+            return new ReverseWordIterator();
+        }
+
+        private class ReverseWordIterator implements Iterator<String> {
+            private int index;
+
+            ReverseWordIterator() {
+                index = string.length - 1;
+                skipSpaces();
+            }
+
+            @Override
+            public boolean hasNext() {
+                return index > 0;
+            }
+
+            @Override
+            public String next() {
+                int end = index;
+                skipToSpace();
+                String s = new String(string, index + 1, end - index);
+                skipSpaces();
+                return s;
+            }
+
+            private void skipSpaces() {
+                while (index >= 0 && string[index] == ' ') {
+                    index--;
+                }
+            }
+
+            private void skipToSpace() {
+                while (index >= 0 && string[index] != ' ') {
+                    index--;
+                }
+            }
+        }
+    }
+
+
 }
--- src/test/java/ru/pchurzin/unilecs/Task014Test.java
+++ src/test/java/ru/pchurzin/unilecs/Task014Test.java
@@ -2,12 +2,27 @@ package ru.pchurzin.unilecs;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static ru.pchurzin.unilecs.Task014.getWordsInReverseOrder;
 
 public class Task014Test {
     @Test
     public void test() {
         assertArrayEquals(new String[]{"third", "second", "first"}, getWordsInReverseOrder("first second third"));
+    }
+
+    @Test
+    public void testWithIterator() {
+        List<String> expected = Arrays.asList("third", "second", "first");
+        List<String> actual = new ArrayList<>();
+        for (String s : new Task014.WordReverser("first second third".toCharArray())) {
+            actual.add(s);
+        }
+        assertEquals(expected, actual);
     }
 }
